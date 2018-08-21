4a0dd9ea8c35dfb0686cf802826f83e7722a5fac
funnycoderstar <funnycoderstar@gmail.com>
2018-08-21 23:19:47 +0800

    feat: 添加levelOrderBottom思路解析

--- src/107-levelOrderBottom.js
+++ src/107-levelOrderBottom.js
@@ -9,6 +9,7 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+// 二叉树的层序遍历要用到迭代法，设置一个队列queue（其实就是数组），把根节点加入队列中，当队列不会空时，出队列元素，然后把这个元素的子结点加入到队列中。
 var levelOrderBottom = function(root) {
     var queue = [];
     var result = [];
