8d87212c4c615ddfd106611d8b3be160c0de9bff
funnycoderstar <funnycoderstar@gmail.com>
2018-10-23 22:49:20 +0800

    feat:   添加889-constructFromPrePost

---
+++ src/889-constructFromPrePost.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} pre
+ * @param {number[]} post
+ * @return {TreeNode}
+ */
+var constructFromPrePost = function(pre, post) {
+    
+};
