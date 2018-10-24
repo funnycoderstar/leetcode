ef7d0b443087a1e763372314b8bc10826daf2e1d
funnycoderstar <funnycoderstar@gmail.com>
2018-10-24 22:52:54 +0800

    feat: 894-allPossibleFBT

---
+++ src/894-allPossibleFBT.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number} N
+ * @return {TreeNode[]}
+ */
+var allPossibleFBT = function(N) {
+    if(N === 1) {
+        return new TreeNode(0);
+    };
+};
