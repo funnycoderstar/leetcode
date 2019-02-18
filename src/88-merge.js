581b2ba618bc7929b5a76c346da00c1f1128f939
funnycoderstar <funnycoderstar@gmail.com>
2019-02-18 23:27:18 +0800

    feat: 合并两个有序数组添加其他解法

--- src/88-merge.js
+++ src/88-merge.js
@@ -13,3 +13,35 @@ var merge = function(nums1, m, nums2, n) {
         return a - b;
     })
 };
+
+
+var merge = function(nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    let p = m + n - 1;
+    while(j >= 0) {
+        if(nums1[i] > nums2[j] && i >= 0) {
+            nums1[p] = nums1[i];
+            i--;
+        } else {
+            nums1[p] = nums2[j];
+            j--;
+        }
+        p--;
+    }
+    return nums1;
+};
+
+var merge = function(nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    while( i>=0 || j >=0) {
+        if(nums1[i] >= nums2[j] || j < 0) {
+            nums1[i+j+1] = nums1[i];
+            i--;
+        } else {
+            nums1[i+j+1] = nums2[j];
+            j--;
+        }
+    }
+};
